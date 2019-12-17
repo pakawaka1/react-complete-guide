8a2b292be9913cd15e1b1e63076dda3ddf58ff2a
pakawaka1 <davidcervantesdev@gmail.com>
2019-12-16 18:10:37 -0800

    feat: first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 30 }
+    ]
+  };
+
+  switchNameHandler = () => {
+    console.log('Was clicked!');
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>I'm a react app!</h1>
+        <p>This is really working! </p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
+          My Hobbies: eat hot dogs and racing.
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const person = props => {
+  return (
+    <p>
+      I'm {props.name}and I am {props.age} years old!
+      {/* {props.children} */}
+    </p>
+  );
+};
+
+export default person;
