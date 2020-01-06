ad17542ab8e740d219e1024071ebcacefb6854ee
pakawaka1 <davidcervantesdev@gmail.com>
2020-01-06 13:16:01 -0800

    feat:  added click event to update DOM with using props

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const person = props => {
   return (
-    <p>
+    <p onClick={props.click}>
       I'm {props.name} and I am {props.age} years old!
       {/* {props.children} */}
     </p>
