ff64f2882ada093769dd416a6661c8e678577fae
pakawaka1 <davidcervantesdev@gmail.com>
2020-01-07 13:53:27 -0800

    feat: first commit

--- src/App.js
+++ src/App.js
@@ -1,76 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person';
+
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
 
 class App extends Component {
   state = {
-    persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 30 }
-    ],
-    otherState: 'some other value'
+    username: 'Supermax'
   };
-
-  switchNameHandler = newName => {
-    this.setState({
-      persons: [
-        { name: newName, age: 500 },
-        { name: 'Manu', age: 29 },
-        { name: 'El Guey', age: 30333 }
-      ]
-    });
+  usernameChangedHandler = event => {
+    this.setState({ username: event.target.value });
   };
-
-  nameChangedHandler = event => {
-    this.setState({
-      persons: [
-        { name: 'Max', age: 500 },
-        { name: event.target.value, age: 29 },
-        { name: 'El Guey', age: 30333 }
-      ]
-    });
-  };
-
   render() {
-    const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1x solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
     return (
       <div className='App'>
-        <h1>I'm a react app!</h1>
-        <p>This is really working!</p>
-        <button
-          style={style}
-          onClick={this.switchNameHandler.bind(this, 'Maximo')}
-        >
-          Switch Name
-        </button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-          click={this.switchNameHandler}
+        <UserInput
+          changed={this.usernameChangedHandler}
+          currentName={this.state.username}
         />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'El Cholos')}
-          changed={this.nameChangedHandler}
-        >
-          My Hobbies: eat hot dogs and racing.
-        </Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-          click={this.switchNameHandler}
-        />
+        <UserOutput userName={this.state.username} />
+        <UserOutput userName={this.state.username} />
+        <UserOutput userName='Max' />
       </div>
     );
   }
 }
-
 export default App;
---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const userOutput = props => {
+  return (
+    <div>
+      <p>Username: {props.userName}</p>
+      <p>Somemore random words!</p>
+    </div>
+  );
+};
+
+export default userOutput;
